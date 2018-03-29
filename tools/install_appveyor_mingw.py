5354481cf6e856f1586985c0736330c6d7f19257
Dario Izzo <dario.izzo@gmail.com>
2018-03-29 15:39:05 +0200

    CI fix for appveyor #10

--- tools/install_appveyor_mingw.py
+++ tools/install_appveyor_mingw.py
@@ -126,7 +126,7 @@ if is_python_build:
         run_command(pip + ' install twine')
 
     # Install pygmo
-    os.chdir('c:\\projects\\pagmo2-2.4')
+    os.chdir('c:\\projects\\pagmo2-2.6')
     os.makedirs('build_pygmo')
     os.chdir('build_pygmo')
     run_command(r'cmake -G "MinGW Makefiles" ..  -DCMAKE_PREFIX_PATH=c:\\local -DCMAKE_INSTALL_PREFIX=c:\\local -DPAGMO_BUILD_PYGMO=yes -DPAGMO_BUILD_PAGMO=no -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-s  -DPYTHON_EXECUTABLE=C:\\Python' + python_version + r'\\python.exe -DPYTHON_LIBRARY=C:\\Python' + python_version +
