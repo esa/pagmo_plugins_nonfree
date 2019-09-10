77f7d90f9e3ce24de612e91d715be120041f90ee
Dario Izzo <dario.izzo@gmail.com>
2019-09-10 15:51:06 +0200

    CI attemp 8 (travis)

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -16,7 +16,7 @@ fi
 if [[ "${PAGMO_PLUGINS_NONFREE_BUILD}" == "ReleaseGCC" ]]; then
     cmake -DCMAKE_PREFIX_PATH=$deps_dir -DBoost_NO_BOOST_CMAKE=ON -DCMAKE_BUILD_TYPE=Release -DPPNF_BUILD_TESTS=yes -DCMAKE_CXX_FLAGS="-fuse-ld=gold" ../;
     make -j2 VERBOSE=1;
-    ctest;
+    ctest -VV;
 elif [[ "${PAGMO_PLUGINS_NONFREE_BUILD}" == "DebugGCC" ]]; then
     cmake -DCMAKE_PREFIX_PATH=$deps_dir -DBoost_NO_BOOST_CMAKE=ON -DCMAKE_BUILD_TYPE=Debug -DPPNF_BUILD_TESTS=yes -DCMAKE_CXX_FLAGS="-fsanitize=address -fuse-ld=gold" ../;
     make -j2 VERBOSE=1;
@@ -24,24 +24,24 @@ elif [[ "${PAGMO_PLUGINS_NONFREE_BUILD}" == "DebugGCC" ]]; then
 elif [[ "${PAGMO_PLUGINS_NONFREE_BUILD}" == "CoverageGCC" ]]; then
     cmake -DCMAKE_PREFIX_PATH=$deps_dir -DBoost_NO_BOOST_CMAKE=ON -DCMAKE_BUILD_TYPE=Debug -DPPNF_BUILD_TESTS=yes -DCMAKE_CXX_FLAGS="--coverage -fuse-ld=gold" ../;
     make -j2 VERBOSE=1;
-    ctest;
+    ctest -VV;
     bash <(curl -s https://codecov.io/bash) -x gcov-5;
 elif [[ "${PAGMO_PLUGINS_NONFREE_BUILD}" == "DebugClang" ]]; then
     CXX=clang++-7 CC=clang-7 cmake -DCMAKE_PREFIX_PATH=$deps_dir -DBoost_NO_BOOST_CMAKE=ON -DCMAKE_BUILD_TYPE=Debug -DPPNF_BUILD_TESTS=yes ../;
     make -j2 VERBOSE=1;
-    ctest;
+    ctest -VV;
 elif [[ "${PAGMO_PLUGINS_NONFREE_BUILD}" == "ReleaseClang" ]]; then
     CXX=clang++-7 CC=clang-7 cmake -DCMAKE_PREFIX_PATH=$deps_dir -DBoost_NO_BOOST_CMAKE=ON -DCMAKE_BUILD_TYPE=Release -DPPNF_BUILD_TESTS=yes ../;
     make -j2 VERBOSE=1;
-    ctest;
+    ctest -VV;
 elif [[ "${PAGMO_PLUGINS_NONFREE_BUILD}" == "OSXDebug" ]]; then
     CXX=clang++ CC=clang cmake -DCMAKE_PREFIX_PATH=$deps_dir -DBoost_NO_BOOST_CMAKE=ON -DCMAKE_BUILD_TYPE=Debug -DPPNF_BUILD_TESTS=yes -DCMAKE_CXX_FLAGS="-g0 -O2" ../;
     make -j2 VERBOSE=1;
-    ctest;
+    ctest -VV;
 elif [[ "${PAGMO_PLUGINS_NONFREE_BUILD}" == "OSXRelease" ]]; then
     CXX=clang++ CC=clang cmake -DCMAKE_PREFIX_PATH=$deps_dir -DBoost_NO_BOOST_CMAKE=ON -DCMAKE_BUILD_TYPE=Release -DPPNF_BUILD_TESTS=yes ../;
     make -j2 VERBOSE=1;
-    ctest;
+    ctest -VV;
 elif [[ "${PAGMO_PLUGINS_NONFREE_BUILD}" == Python* ]]; then
     export CXX=g++-4.8
     export CC=gcc-4.8
