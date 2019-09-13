fb32d8e37d5833a3859881d4f3b63406bef7ab15
Dario Izzo <dario.izzo@gmail.com>
2019-09-13 13:17:03 +0200

    bug fix

--- tools/install_docker.sh
+++ tools/install_docker.sh
@@ -6,67 +6,6 @@ set -x
 # Exit on error.
 set -e
 
-PAGMO_VERSION="2.11.3"
-
-if [[ ${PAGMO_PLUGINS_NONFREE_BUILD} == *37 ]]; then
-	PYTHON_DIR="cp37-cp37m"
-elif [[ ${PAGMO_PLUGINS_NONFREE_BUILD} == *36 ]]; then
-	PYTHON_DIR="cp36-cp36m"
-elif [[ ${PAGMO_PLUGINS_NONFREE_BUILD} == *35 ]]; then
-	PYTHON_DIR="cp35-cp35m"
-elif [[ ${PAGMO_PLUGINS_NONFREE_BUILD} == *27 ]]; then
-	PYTHON_DIR="cp27-cp27mu"
-else
-	echo "Invalid build type: ${PAGMO_PLUGINS_NONFREE_BUILD}"
-	exit 1
-fi
-
-# HACK: for python 3.x, the include directory
-# is called 'python3.xm' rather than just 'python3.x'.
-# This confuses the build system of Boost.Python, thus
-# we create a symlink to 'python3.x'.
-cd /opt/python/${PYTHON_DIR}/include
-PY_INCLUDE_DIR_NAME=`ls`
-# If the include dir ends with 'm', create a symlink
-# without the 'm'.
-if [[ $PY_INCLUDE_DIR_NAME == *m ]]; then
-	ln -s $PY_INCLUDE_DIR_NAME `echo $PY_INCLUDE_DIR_NAME|sed 's/.$//'`
-fi
-
-cd
-cd install
-
-# Python deps (numpy is needed to install pygmo)
-/opt/python/${PYTHON_DIR}/bin/pip install cloudpickle numpy
-
-# pagmo & pygmo
-curl -L  https://github.com/esa/pagmo2/archive/v${PAGMO_VERSION}.tar.gz > pagmo2.tar.gz
-tar xzf pagmo2.tar.gz
-cd pagmo2-${PAGMO_VERSION}
-mkdir build_pagmo
-cd build_pagmo
-cmake ../ -DBoost_NO_BOOST_CMAKE=ON -DPAGMO_WITH_EIGEN3=yes -DPAGMO_WITH_NLOPT=yes -DCMAKE_BUILD_TYPE=Release
-make install
-cd ..
-mkdir build_pygmo 
-cd build_pygmo
-cmake -DCMAKE_BUILD_TYPE=Release -DBoost_NO_BOOST_CMAKE=ON -DPAGMO_BUILD_PYGMO=yes -DPAGMO_BUILD_PAGMO=no -DPYTHON_EXECUTABLE=/opt/python/${PYTHON_DIR}/bin/python ../;
-make -j2 install
-
-
-
-
-
-
-
-#!/usr/bin/env bash
-
-# Echo each command
-set -x
-
-# Exit on error.
-set -e
-
 if [[ ${PAGMO_PLUGINS_NONFREE_BUILD} == *37 ]]; then
 	PYTHON_DIR="cp37-cp37m"
 	BOOST_PYTHON_LIBRARY_NAME="libboost_python37.so"
