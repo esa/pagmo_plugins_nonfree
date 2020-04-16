a5e4d4b839e9eee27d0aac47ec02678720ea52d1
Dario Izzo <dario.izzo@gmail.com>
2020-04-16 12:29:50 +0200

    trials

--- tools/install_deps.sh
+++ tools/install_deps.sh
@@ -9,9 +9,9 @@ set -e
 # For the non manylinux builds (i.e. pip) we use conda and thus install and activate a conda environment
 if [[ "${PAGMO_PLUGINS_NONFREE_BUILD}" != manylinux* ]]; then
     if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
-        wget https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh -O miniconda.sh;
+        wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
     else
-        wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
+        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
     fi
     export deps_dir=$HOME/local
     export PATH="$HOME/miniconda/bin:$PATH"
@@ -19,16 +19,16 @@ if [[ "${PAGMO_PLUGINS_NONFREE_BUILD}" != manylinux* ]]; then
     conda config --add channels conda-forge --force
 
     # All builds will need these
-    conda_pkgs="boost cmake pagmo-devel"
+    conda_pkgs="boost cmake pagmo-devel cxx-compiler"
 
     # Only Python builds will need these
     if [[ "${PAGMO_PLUGINS_NONFREE_BUILD}" == "Python37" || "${PAGMO_PLUGINS_NONFREE_BUILD}" == "OSXPython37" ]]; then
         conda_pkgs="$conda_pkgs python=3.7 pygmo pybind11" 
     fi
 
-    if [[ "${PAGMO_PLUGINS_NONFREE_BUILD}" == OSX* ]]; then
-        conda_pkgs="$conda_pkgs clangdev<10"
-    fi
+    #if [[ "${PAGMO_PLUGINS_NONFREE_BUILD}" == OSX* ]]; then
+    #    conda_pkgs="$conda_pkgs clangdev<10"
+    #fi
 
     if [[ "${PAGMO_PLUGINS_NONFREE_BUILD}" == Python* ]]; then
         conda_pkgs="$conda_pkgs graphviz doxygen sphinx breathe"
