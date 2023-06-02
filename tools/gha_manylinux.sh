1497b507382ea2ccffe3d1c5fdaa368380d105de
Francesco Biscani <bluescarni@gmail.com>
2023-06-02 15:04:36 +0200

    WIP.

--- tools/gha_manylinux.sh
+++ tools/gha_manylinux.sh
@@ -7,7 +7,7 @@ set -x
 set -e
 
 # Report on the environrnt variables used for this build
-echo "PYGMO_BUILD_TYPE: ${PYGMO_BUILD_TYPE}"
+echo "PPNF_BUILD_TYPE: ${PPNF_BUILD_TYPE}"
 echo "GITHUB_REF: ${GITHUB_REF}"
 echo "GITHUB_WORKSPACE: ${GITHUB_WORKSPACE}"
 # No idea why but this following line seems to be necessary (added: 18/01/2023)
@@ -17,37 +17,38 @@ echo "BRANCH_NAME: ${BRANCH_NAME}"
 
 
 # 1 - We read for what python wheels have to be built
-if [[ ${PYGMO_BUILD_TYPE} == *38* ]]; then
+if [[ ${PPNF_BUILD_TYPE} == *38* ]]; then
 	PYTHON_DIR="cp38-cp38"
-elif [[ ${PYGMO_BUILD_TYPE} == *39* ]]; then
+elif [[ ${PPNF_BUILD_TYPE} == *39* ]]; then
 	PYTHON_DIR="cp39-cp39"
-elif [[ ${PYGMO_BUILD_TYPE} == *310* ]]; then
+elif [[ ${PPNF_BUILD_TYPE} == *310* ]]; then
 	PYTHON_DIR="cp310-cp310"
-elif [[ ${PYGMO_BUILD_TYPE} == *311* ]]; then
+elif [[ ${PPNF_BUILD_TYPE} == *311* ]]; then
 	PYTHON_DIR="cp311-cp311"
-elif [[ ${PYGMO_BUILD_TYPE} == *312* ]]; then
+elif [[ ${PPNF_BUILD_TYPE} == *312* ]]; then
 	PYTHON_DIR="cp312-cp312"
 else
-	echo "Invalid build type: ${PYGMO_BUILD_TYPE}"
+	echo "Invalid build type: ${PPNF_BUILD_TYPE}"
 	exit 1
 fi
 
 # Report the inferred directory where python is found
 echo "PYTHON_DIR: ${PYTHON_DIR}"
 
-# The pagmo version to be used for releases.
-export PAGMO_VERSION_RELEASE="2.19.0"
+# The pagmo/pygmo versions to be used.
+export PAGMO_VERSION="2.19.0"
+export PYGMO_VERSION="2.19.5"
 
 # Check if this is a release build.
 if [[ "${GITHUB_REF}" == "refs/tags/v"* ]]; then
     echo "Tag build detected"
-	export PYGMO_RELEASE_BUILD="yes"
+	export PPNF_RELEASE_BUILD="yes"
 else
 	echo "Non-tag build detected"
 fi
 
 # Python mandatory deps.
-/opt/python/${PYTHON_DIR}/bin/pip install cloudpickle numpy
+/opt/python/${PYTHON_DIR}/bin/pip install cloudpickle numpy pygmo==${PYGMO_VERSION}
 # Python optional deps.
 /opt/python/${PYTHON_DIR}/bin/pip install dill==0.3.5.1 networkx ipyparallel scipy
 
@@ -56,14 +57,9 @@ fi
 cd /root/install
 
 # Install pagmo
-if [[ "${PYGMO_RELEASE_BUILD}" == "yes" ]]; then
-	curl -L -o pagmo2.tar.gz https://github.com/esa/pagmo2/archive/refs/tags/v${PAGMO_VERSION_RELEASE}.tar.gz
-	tar xzf pagmo2.tar.gz
-	cd pagmo2-${PAGMO_VERSION_RELEASE}
-else
-	git clone https://github.com/esa/pagmo2.git
-	cd pagmo2
-fi
+curl -L -o pagmo2.tar.gz https://github.com/esa/pagmo2/archive/refs/tags/v${PAGMO_VERSION}.tar.gz
+tar xzf pagmo2.tar.gz
+cd pagmo2-*
 
 mkdir build
 cd build
@@ -75,21 +71,30 @@ cmake -DBoost_NO_BOOST_CMAKE=ON \
 	-DCMAKE_BUILD_TYPE=Release ../;
 make -j4 install
 
-# pygmo
+# pygmo_plugins_nonfree
 cd ${GITHUB_WORKSPACE}
-mkdir build
-cd build
-cmake -DBoost_NO_BOOST_CMAKE=ON \
-	-DCMAKE_BUILD_TYPE=Release \
-	-DPYGMO_ENABLE_IPO=ON \
-	-DPython3_EXECUTABLE=/opt/python/${PYTHON_DIR}/bin/python ../;
+mkdir build_pagmo_plugins_nonfree
+cd build_pagmo_plugins_nonfree
+cmake -DCMAKE_BUILD_TYPE=Release \
+	-DBoost_NO_BOOST_CMAKE=ON \
+	-DPPNF_BUILD_CPP=yes \
+	-DPPNF_BUILD_TESTS=no \
+	-DPPNF_BUILD_PYTHON=no ../;
 make -j2 install
+cd ..
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release \
+	-DBoost_NO_BOOST_CMAKE=ON \
+	-DPPNF_BUILD_CPP=no \
+	-DPPNF_BUILD_PYTHON=yes \
+	-DPYTHON_EXECUTABLE=/opt/python/${PYTHON_DIR}/bin/python ../;
+make -j2 install 
 
 # Making the wheel and installing it
 cd wheel
-# Copy the installed pygmo files, wherever they might be in /usr/local,
+# Copy the installed ppnf files, wherever they might be in /usr/local,
 # into the current dir.
-cp -r ../pygmo ./
+cp -a `find /usr/local/lib -type d -iname 'pygmo_plugins_nonfree'` ./
 # Create the wheel and repair it.
 /opt/python/${PYTHON_DIR}/bin/python setup.py bdist_wheel
 auditwheel repair dist/pygmo* -w ./dist2
@@ -114,7 +119,7 @@ sleep 20
 # fi
 
 # Upload to PyPI.
-if [[ "${PYGMO_RELEASE_BUILD}" == "yes" ]]; then
+if [[ "${PPNF_RELEASE_BUILD}" == "yes" ]]; then
 	/opt/python/${PYTHON_DIR}/bin/pip install twine
 	/opt/python/${PYTHON_DIR}/bin/twine upload -u ci4esa ${GITHUB_WORKSPACE}/build/wheel/dist2/pygmo*
 fi
